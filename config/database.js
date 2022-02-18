5337f60ea303b6052976a5085a55e27f9930f457
Dressa <andressamendeslima8@gmail.com>
2022-02-18 14:56:50 -0300

    primeiro repositorio

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+ mongoose.connect("mongodb+srv://Lavinia:99794450@cluster0.m4sum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+module.exports = mongoose;
